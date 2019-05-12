36f021a7cb115dcd3a0746cde6a19ccb8383e8e1
vidur singla <vidursingla13@gmail.com>
2019-05-12 08:51:18 +0530

    Updates

---
+++ precache-manifest.2803deb9a19f0432bbb6ef5cd3c40850.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cca63e92be288f6e0bc0",
+    "url": "/movie-catalog/static/css/main.1402fcc3.chunk.css"
+  },
+  {
+    "revision": "cca63e92be288f6e0bc0",
+    "url": "/movie-catalog/static/js/main.cca63e92.chunk.js"
+  },
+  {
+    "revision": "1411b5809c096cfa68a9",
+    "url": "/movie-catalog/static/js/1.1411b580.chunk.js"
+  },
+  {
+    "revision": "5ffe2f484a4726b98d3e",
+    "url": "/movie-catalog/static/js/runtime~main.5ffe2f48.js"
+  },
+  {
+    "revision": "17780a7a5bfe53017d7621b3628c7bd3",
+    "url": "/movie-catalog/index.html"
+  }
+];
